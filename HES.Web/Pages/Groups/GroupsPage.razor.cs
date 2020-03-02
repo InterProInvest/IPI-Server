c035191754a25488a4e3458a7e39c20a4709811b
Sitler <vitaliy.martinyk@gmail.com>
2020-03-02 12:58:30 +0200

    Added search, fixed pager on imported groups

--- HES.Web/Pages/Groups/GroupsPage.razor.cs
+++ HES.Web/Pages/Groups/GroupsPage.razor.cs
@@ -1,40 +1,35 @@
-﻿using HES.Core.Entities;
+﻿using System;
+using System.Timers;
 using HES.Core.Enums;
-using HES.Core.Interfaces;
-using HES.Core.Models.Web.Breadcrumb;
+using HES.Core.Entities;
 using HES.Web.Components;
-using Microsoft.AspNetCore.Components;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
+using HES.Core.Interfaces;
 using System.ComponentModel;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using HES.Core.Models.Web.Breadcrumb;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace HES.Web.Pages.Groups
 {
     public partial class GroupsPage : ComponentBase
     {
-        #region Di
-
         [Inject] 
         public IGroupService GroupService { get; set; }
         
         [Inject] 
         public ILogger<GroupsPage> Logger { get; set; }
 
-        #endregion
-
-        #region Properties
-        
         public IList<Group> Groups { get; set; }
-        public string CurrentGroupId { get; set; }
-
-        #endregion
-
-        #region LifeCycleHooks
 
         protected override async Task OnInitializedAsync()
         {
+            _timer = new Timer(700);
+            _timer.Elapsed += async (sender, args) => { await FilterRecordsAsync();};
+            _timer.AutoReset = false;
+
             await InitializeComponentAsync();
         }
 
@@ -43,16 +38,17 @@ namespace HES.Web.Pages.Groups
             await CreateBreadcrumbsAsync();
         }
 
-        #endregion
 
         #region SortTable
 
         private bool _isSortedAscending;
         private string _activeSortColumn;
+        public string CurrentGroupId { get; set; }
+
 
         public async Task<IList<Group>> SortRecords(string columnName, ListSortDirection dir)
         {
-            return await GroupService.GetAllGroupsAsync((CurrentPage - 1) * PageSize, PageSize, dir, _searchString, columnName);
+            return await GroupService.GetAllGroupsAsync((CurrentPage - 1) * PageSize, PageSize, dir, SearchString, columnName);
         }
 
         public async Task SortTable(string columnName)
@@ -167,35 +163,46 @@ namespace HES.Web.Pages.Groups
             await LoadGroupsAsync();
         }
 
-        public async Task ShowEntriesOnChange(ChangeEventArgs args)
+        public async Task RefreshTable()
         {
-            PageSize = Convert.ToInt32(args.Value);
+            TotalRecords = await GroupService.GetCountAsync(SearchString);
             TotalPages = (int)Math.Ceiling(TotalRecords / (decimal)PageSize);
             CurrentPage = 1;
             PagerSize = TotalPages < 3 ? TotalPages : 3;
             StartPage = 1;
             EndPage = PagerSize;
-
             await LoadGroupsAsync();
+        }
+
+        public async Task ShowEntriesOnChange(ChangeEventArgs args)
+        {
+            PageSize = Convert.ToInt32(args.Value);
+            await RefreshTable();
         }
 
         #endregion
 
-        private string _searchString;
+        #region Search
 
-        public async Task FilterRecordsAsync(ChangeEventArgs args)
+        private string SearchString { get; set; }
+        private Timer _timer;
+
+        public void HandleKeyUp(KeyboardEventArgs e)
         {
-            _searchString = (string)args.Value;
-
-            TotalRecords = await GroupService.GetCountAsync(_searchString);
-            TotalPages = (int)Math.Ceiling(TotalRecords / (decimal)PageSize);
-            CurrentPage = 1;
-            PagerSize = TotalPages < 3 ? TotalPages : 3;
-            StartPage = 1;
-            EndPage = PagerSize;
-
-            await LoadGroupsAsync();
+            _timer.Stop();
+            _timer.Start();
         }
+
+        public async Task FilterRecordsAsync()
+        {
+            await InvokeAsync(async () =>
+            {
+                await RefreshTable();
+                StateHasChanged();
+            });
+        }
+
+        #endregion
 
         public async Task InitializeComponentAsync()
         {
@@ -203,14 +210,14 @@ namespace HES.Web.Pages.Groups
             PageSize = 10;
             CurrentPage = 1;
             await LoadGroupsAsync();
-            TotalRecords = await GroupService.GetCountAsync(_searchString);
+            TotalRecords = await GroupService.GetCountAsync(SearchString);
             TotalPages = (int)Math.Ceiling(TotalRecords / (decimal)PageSize);
             SetPagerSize(PagerDirection.Next);
         }
 
         public async Task LoadGroupsAsync()
         {
-            Groups = await GroupService.GetAllGroupsAsync((CurrentPage - 1) * PageSize, PageSize, ListSortDirection.Ascending, _searchString);
+            Groups = await GroupService.GetAllGroupsAsync((CurrentPage - 1) * PageSize, PageSize, ListSortDirection.Ascending, SearchString);
             CurrentGroupId = null;
         }
 
@@ -236,7 +243,7 @@ namespace HES.Web.Pages.Groups
             RenderFragment body = (builder) =>
             {
                 builder.OpenComponent(0, typeof(AddGroup));
-                builder.AddAttribute(1, "Refresh", EventCallback.Factory.Create(this, LoadGroupsAsync));
+                builder.AddAttribute(1, "Refresh", EventCallback.Factory.Create(this, RefreshTable));
                 builder.CloseComponent();
             };
 
