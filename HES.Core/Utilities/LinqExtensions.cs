0ebdad57b51c6fa8044afe0046001a17a10fb062
Sitler <vitaliy.martinyk@gmail.com>
2020-03-05 15:25:37 +0200

    Added SearchBox component, fixed group page

--- HES.Core/Utilities/LinqExtensions.cs
+++ HES.Core/Utilities/LinqExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Linq.Expressions;
-using Microsoft.EntityFrameworkCore;
 
 namespace HES.Core.Utilities
 {
@@ -31,74 +29,5 @@ namespace HES.Core.Utilities
 
             return query.Provider.CreateQuery<T>(resultExpression);
         }
-
-        public static async Task<int> SearchCountAsync<T>(this IQueryable<T> query, string search = null)
-        {
-            return await Task.Run(async () =>
-            {
-                if (string.IsNullOrWhiteSpace(search))
-                {
-                    return await query.CountAsync();
-                }
-
-                search = search.Trim().ToLower();
-                int count = 0;
-
-                foreach (var item in query)
-                {
-                    foreach (var property in item.GetType().GetProperties().Where(p => p.Name.ToLower() == "name" || p.Name.ToLower() == "email" || p.Name.ToLower() == "description"))
-                    {
-                        var propValue = property.GetValue(item)?.ToString();
-
-                        if (propValue == null)
-                            continue;
-
-                        var isContains = propValue.ToLower().Contains(search);
-                        if (isContains)
-                        {
-                            count++;
-                            break;
-                        }
-                    }
-                }
-
-                return count;
-            });
-        }
-
-        //public static IQueryable<T> WhereSearch<T>(this IQueryable<T> query, string search = null)
-        //{
-        //    //if (string.IsNullOrWhiteSpace(search))
-        //    //    return query;
-
-        //    //query = query.Where(x => );
-
-        //    //var a = await query.ToListAsync();
-
-        //    //return query;
-
-        //    //if (string.IsNullOrWhiteSpace(search))
-        //    //    return query;
-
-        //    //search = search.Trim().ToLower();
-        //    //var resultList = new List<T>();
-
-        //    //foreach (var item in query)
-        //    //{
-        //    //    foreach (var property in item.GetType().GetProperties())
-        //    //    {
-        //    //        var propValue = property.GetValue(item)?.ToString();
-
-        //    //        if (propValue == null)
-        //    //            continue;
-
-        //    //        var isContains = propValue.ToLower().Contains(search);
-        //    //        if (isContains)
-        //    //            resultList.Add(item);
-        //    //    }
-        //    //}
-
-        //    //return resultList.AsQueryable();
-        //}
     }
 }
---
+++ HES.Web/Components/SearchBox.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace HES.Web.Components
+{
+    public partial class SearchBox : ComponentBase
+    {
+        [Parameter] public int Delay { get; set; } = 500;
+        [Parameter] public Func<string, Task> Search { get; set; }
+        [Parameter] public string Placeholder { get; set; } = "Search";
+
+        public string SearchString { get; set; }
+        private Timer _timer;
+
+        protected override void OnInitialized()
+        {
+            _timer = new Timer(Delay);
+            _timer.Elapsed += async (sender, args) =>
+            {
+                await InvokeAsync(async () =>
+                {
+                    await Search.Invoke(SearchString.Trim());
+                    StateHasChanged();
+                });
+            };
+            _timer.AutoReset = false;
+        }
+
+        public void OnKeyUp(KeyboardEventArgs e)
+        {
+            _timer.Stop();
+            _timer.Start();
+        }
+    }
+}
