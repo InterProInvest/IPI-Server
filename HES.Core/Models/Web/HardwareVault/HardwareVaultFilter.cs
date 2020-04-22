2d2630ad553f7cde50c3c422b831005e10dfe644
Sitler <vitaliy.martinyk@gmail.com>
2020-04-22 20:26:23 +0300

    Deleted SyncDevices component, added filter, added searchbox

--- HES.Core/Models/Web/HardwareVault/HardwareVaultFilter.cs
+++ HES.Core/Models/Web/HardwareVault/HardwareVaultFilter.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using HES.Core.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace HES.Core.Models.Web.HardwareVault
 {
     public class HardwareVaultFilter
     {
-        public int MyProperty { get; set; }
+        [Display(Name = "Start Date")]
+        public DateTime? StartDate { get; set; }
+        [Display(Name = "End Date")]
+        public DateTime? EndDate { get; set; }
+        [Range(0, 100)]
+        public int? Battery { get; set; }
+        public string Firmware { get; set; }
+        public LicenseStatus? LicenseStatus { get; set; }
+        [Display(Name = "Employee")]
+        public string EmployeeId { get; set; }
+        [Display(Name = "Company")]
+        public string CompanyId { get; set; }
+        [Display(Name = "Department")]
+        public string DepartmentId { get; set; }
+        public int Records { get; set; }
     }
 }
--- HES.Web/Pages/Devices/HardwareVaultsPage.razor.cs
+++ HES.Web/Pages/Devices/HardwareVaultsPage.razor.cs
@@ -1,18 +1,47 @@
 ﻿using HES.Core.Entities;
+using HES.Core.Enums;
 using HES.Core.Interfaces;
 using HES.Core.Models.Web.HardwareVault;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HES.Web.Pages.Devices
 {
     public partial class HardwareVaultsPage : ComponentBase
     {
+        #region DependencyInjections
+
+        [Inject]
+        IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        IToastService ToastService { get; set; }
+
+        [Inject]
+        IDeviceService DeviceService { get; set; }
+
+        [Inject]
+        ILogger<HardwareVaultsPage> Logger { get; set; }
+
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
         [Inject]
         public IDeviceService HardwareVaultService { get; set; }
+
+        [Inject]
+        public IOrgStructureService OrgStructureService { get; set; }
+
+        #endregion
 
         public List<Device> HardwareVaults { get; set; }
         public Device SelectedHardwareVault { get; set; }
@@ -29,6 +58,21 @@ namespace HES.Web.Pages.Devices
         {
             await LoadTableDataAsync();
         }
+
+
+        public SelectList FilterFirmwares { get; set; }
+        public SelectList FilterLicenseStatuses { get; set; }
+        public SelectList FilterEmployees { get; set; }
+        public SelectList FilterCompanies { get; set; }
+
+        public async Task InitializeFilterAsync()
+        {
+            FilterFirmwares = new SelectList(HardwareVaults.Select(s => s.Firmware).Distinct().OrderBy(f => f).ToDictionary(t => t, t => t), "Key", "Value");
+            FilterLicenseStatuses = new SelectList(Enum.GetValues(typeof(LicenseStatus)).Cast<LicenseStatus>().ToDictionary(t => (int)t, t => t.ToString()), "Key", "Value");
+            FilterEmployees = new SelectList(await EmployeeService.EmployeeQuery().OrderBy(e => e.FirstName).ThenBy(e => e.LastName).ToListAsync(), "Id", "FullName");
+            FilterCompanies = new SelectList(await OrgStructureService.CompanyQuery().ToListAsync(), "Id", "Name");
+        }
+
 
         private async Task LoadTableDataAsync()
         {
@@ -78,5 +122,41 @@ namespace HES.Web.Pages.Devices
             CurrentPage = 1;
             await LoadTableDataAsync();
         }
+
+        private async Task SearchTextChangedAsync(string searchText)
+        {
+            SearchText = searchText;
+            await LoadTableDataAsync();
+        }
+
+        private async Task ClearAsync()
+        {
+            Filter = new HardwareVaultFilter();
+            await LoadTableDataAsync();
+        }
+
+        #region TableActions
+
+        public async Task SynchronizeDevicesAsync()
+        {
+            await JSRuntime.InvokeVoidAsync("showSpinner", "syncBtnSpinner");
+
+            try
+            {
+                await DeviceService.ImportDevicesAsync();
+                await LoadTableDataAsync();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+                ToastService.ShowToast("Somethings went wrong.", ToastLevel.Error);
+            }
+            finally
+            {
+                await JSRuntime.InvokeVoidAsync("showSpinner", "syncBtnSpinner");
+            }
+        }
+
+        #endregion
     }
 }
