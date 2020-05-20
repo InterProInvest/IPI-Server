314bbcb66e4471ddaf4fb4e233a6bf9771b11faf
Sitler <vitaliy.martinyk@gmail.com>
2020-05-20 16:07:46 +0300

    Added tests on employee service EditEmployeeAsync method

---
+++ HES.Tests/EmployeeServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using Xunit;
+using System;
+using HES.Core.Services;
+using HES.Core.Entities;
+using HES.Core.Exceptions;
+using HES.Core.Interfaces;
+using System.Threading.Tasks;
+
+namespace HES.Tests
+{
+    public class EmployeeServiceTests
+    {
+        [Fact]
+        public async Task EditEmployeeAsync_NoEmployeeExistsSholdFail()
+        {
+            var employee = new Employee();
+
+            var mock = new Mock<IAsyncRepository<Employee>>();
+            mock.Setup(r => r.ExistAsync(x => x.FirstName == employee.FirstName && x.LastName == employee.LastName && x.Id != employee.Id)).Returns(async () => 
+            {
+                await Task.CompletedTask;
+                return true;
+            });
+            var employeeService = new EmployeeService(mock.Object, null,null, null, null, null, null, null, null, null);
+
+
+            await Assert.ThrowsAsync<AlreadyExistException>(async () => await employeeService.EditEmployeeAsync(employee));
+        }
+
+        [Fact]
+        public async Task EditEmployeeAsync_EmployeeIsNullSholdFail()
+        {
+            var mock = new Mock<IAsyncRepository<Employee>>();
+
+            var employeeService = new EmployeeService(mock.Object, null, null, null, null, null, null, null, null, null);
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await employeeService.EditEmployeeAsync(null));
+        }
+    }
+}
