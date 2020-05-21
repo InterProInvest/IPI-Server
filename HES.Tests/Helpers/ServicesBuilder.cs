9d08ba5a0823f22f0747323c7b5c218d372ac148
Sitler <vitaliy.martinyk@gmail.com>
2020-05-21 14:32:58 +0300

    Added serviceBuilder

---
+++ HES.Tests/Helpers/ServicesBuilder.cs
@@ -0,0 +1,28 @@
+﻿using HES.Core.Entities;
+using HES.Core.Services;
+using HES.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace HES.Tests.Helpers
+{
+    public class ServicesBuilder
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ServicesBuilder(string dbName)
+        {
+            var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(dbName)
+                .Options;
+​
+            _dbContext = new ApplicationDbContext(dbOptions);
+        }
+
+        public EmployeeService GetEmployeeService()
+        {
+            var _empRepository = new Repository<Employee>(_dbContext);
+
+            return new EmployeeService(_empRepository, null, null, null, null, null, null, null, null, null);
+        }
+    }
+}
