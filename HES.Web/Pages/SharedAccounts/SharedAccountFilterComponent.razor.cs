a172dff2750f330273ed1991db612ce3e5f223b6
Sitler <vitaliy.martinyk@gmail.com>
2020-06-22 10:19:39 +0300

    Added SharedAccountFilter component

---
+++ HES.Web/Pages/SharedAccounts/SharedAccountFilterComponent.razor.cs
@@ -0,0 +1,28 @@
+﻿using HES.Core.Interfaces;
+using HES.Core.Models.Web.SharedAccounts;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace HES.Web.Pages.SharedAccounts
+{
+    public partial class SharedAccountFilterComponent : ComponentBase
+    {
+        [Inject] public ISharedAccountService SharedAccountService { get; set; }
+        [Parameter] public Func<SharedAccountsFilter, Task> FilterChanged { get; set; }
+
+        public SharedAccountsFilter Filter { get; set; } = new SharedAccountsFilter();
+
+
+        private async Task FilteredAsync()
+        {
+            await FilterChanged.Invoke(Filter);
+        }
+
+        private async Task ClearAsync()
+        {
+            Filter = new SharedAccountsFilter();
+            await FilterChanged.Invoke(Filter);
+        }
+    }
+}
