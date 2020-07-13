3a39c9d2b8de85898ae71d51cbd36d006a0706f5
CORE <namespacedevbox@gmail.com>
2020-07-13 20:07:55 +0300

    Code cleanup

--- HES.Web/wwwroot/js/main.js
+++ HES.Web/wwwroot/js/main.js
@@ -1,4 +1,44 @@
-﻿function createBreadcrumbs(breadcrumbs) {
+﻿$(document).ready(function () {
+    $('.loading').removeClass('d-flex');
+    $('.loading').addClass('d-none');
+
+    var toggled = localStorage.getItem('ToggledSidebar');
+    if (toggled === 'true') {
+        ToggleSidebarClass();
+    }
+
+    $('#collapseAudit').on('show.bs.collapse', function () {
+        $('#collapseSettings').collapse('hide');
+    });
+
+    $('#collapseSettings').on('show.bs.collapse', function () {
+        $('#collapseAudit').collapse('hide');
+    });
+});
+
+function ToggleSidebar() {
+    ToggleSidebarClass();
+
+    var toggled = localStorage.getItem('ToggledSidebar');
+    if (toggled === 'true') {
+        localStorage.setItem('ToggledSidebar', 'false');
+    }
+    else {
+        localStorage.setItem('ToggledSidebar', 'true');
+    }
+}
+
+function ToggleSidebarClass() {
+    $('.custom-sidebar').toggleClass('toggled');
+    $('.page_labels').toggleClass('toggled');
+    $('.sidebar-settings').toggleClass('toggled');
+    $('.icon-arrow').toggleClass('toggled');
+    $('.copyright').toggleClass('toggled');
+    $('.loading').toggleClass('toggled');
+    $('.icon-hideez').toggleClass('toggled');
+}
+
+function createBreadcrumbs(breadcrumbs) {
     $('#breadcrumb').toggleClass('d-none');
     for (var i = 0; i < breadcrumbs.length; i++) {
         if (breadcrumbs[i].active) {
