5a9745f4fddabb567bd00ba5b8e321c1945579e0
CORE <namespacedevbox@gmail.com>
2020-07-21 09:04:30 +0300

    Added ByDepartmentsTab

---
+++ HES.Web/Pages/Audit/WorkstationSummaries/ByDepartmentsTab.razor.cs
@@ -0,0 +1,18 @@
+﻿using HES.Core.Interfaces;
+using HES.Core.Models.Web.Audit;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace HES.Web.Pages.Audit.WorkstationSummaries
+{
+    public partial class ByDepartmentsTab : ComponentBase
+    {
+        [Inject] public IMainTableService<SummaryByDepartments, SummaryFilter> MainTableService { get; set; }
+        [Inject] public IWorkstationAuditService WorkstationAuditService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await MainTableService.InitializeAsync(WorkstationAuditService.GetSummaryByDepartmentsAsync, WorkstationAuditService.GetSummaryByDepartmentsCountAsync, StateHasChanged, nameof(SummaryByDepartments.Company), syncPropName: "Company");
+        }
+    }
+}
