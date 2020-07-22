8d7ed75b2b0361bfeca6ebf77889524c9450f3a8
CORE <namespacedevbox@gmail.com>
2020-07-22 14:17:57 +0300

    Fixed audit api

--- HES.Web/Controllers/AuditController.cs
+++ HES.Web/Controllers/AuditController.cs
@@ -1,6 +1,6 @@
 ﻿using HES.Core.Entities;
 using HES.Core.Interfaces;
-using HES.Core.Models;
+using HES.Core.Models.Web;
 using HES.Core.Models.Web.Audit;
 using HES.Infrastructure;
 using Microsoft.AspNetCore.Authorization;
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Threading.Tasks;
 
 namespace HES.Web.Controllers
@@ -30,105 +31,186 @@ namespace HES.Web.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<WorkstationEvent>>> GetEvents()
         {
-            //return await _workstationAuditService.GetWorkstationEventsAsync();
-            var list = new List<WorkstationEvent>();
-            return list;
+            var count = await _workstationAuditService.GetWorkstationEventsCountAsync(new DataLoadingOptions<WorkstationEventFilter>());
+            return await _workstationAuditService.GetWorkstationEventsAsync(new DataLoadingOptions<WorkstationEventFilter>
+            {
+                Take = count,
+                SortedColumn = nameof(WorkstationEvent.Date),
+                SortDirection = ListSortDirection.Descending
+            });
         }
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<WorkstationEvent>>> GetFilteredEvents(WorkstationEventFilter workstationEventFilter)
         {
-            //return await _workstationAuditService.GetFilteredWorkstationEventsAsync(workstationEventFilter);
-            var list = new List<WorkstationEvent>();
-            return list;
+            var count = await _workstationAuditService.GetWorkstationEventsCountAsync(new DataLoadingOptions<WorkstationEventFilter>
+            {
+                Filter = workstationEventFilter
+            });
+
+            return await _workstationAuditService.GetWorkstationEventsAsync(new DataLoadingOptions<WorkstationEventFilter>
+            {
+                Take = count,
+                SortedColumn = nameof(WorkstationEvent.Date),
+                SortDirection = ListSortDirection.Descending,
+                Filter = workstationEventFilter
+            }); 
         }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<WorkstationSession>>> GetSessions()
         {
-            //return await _workstationAuditService.GetWorkstationSessionsAsync();
-            var list = new List<WorkstationSession>();
-            return list;
+            var count = await _workstationAuditService.GetWorkstationSessionsCountAsync(new DataLoadingOptions<WorkstationSessionFilter>());
+            return await _workstationAuditService.GetWorkstationSessionsAsync(new DataLoadingOptions<WorkstationSessionFilter>
+            {
+                Take = count,
+                SortedColumn = nameof(WorkstationSession.StartDate),
+                SortDirection = ListSortDirection.Descending
+            });
         }
 
-        //[HttpPost]
-        //[ProducesResponseType(StatusCodes.Status200OK)]
-        //public async Task<ActionResult<IEnumerable<WorkstationSession>>> GetFilteredSessions(WorkstationSessionFilter workstationSessionFilter)
-        //{
-        //    return await _workstationAuditService.GetFilteredWorkstationSessionsAsync(workstationSessionFilter);
-        //}
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<WorkstationSession>>> GetFilteredSessions(WorkstationSessionFilter workstationSessionFilter)
+        {
+            var count = await _workstationAuditService.GetWorkstationSessionsCountAsync(new DataLoadingOptions<WorkstationSessionFilter>
+            {
+                Filter = workstationSessionFilter
+            });
+
+            return await _workstationAuditService.GetWorkstationSessionsAsync(new DataLoadingOptions<WorkstationSessionFilter>
+            {
+                Take = count,
+                SortedColumn = nameof(WorkstationSession.StartDate),
+                SortDirection = ListSortDirection.Descending,
+                Filter = workstationSessionFilter
+            });
+        }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<SummaryByDayAndEmployee>>> GetSummaryByDayAndEmployees()
         {
-            //return await _workstationAuditService.GetSummaryByDayAndEmployeesAsync();
-            var list = new List<SummaryByDayAndEmployee>();
-            return list;
+            var count = await _workstationAuditService.GetSummaryByDayAndEmployeesCountAsync(new DataLoadingOptions<SummaryFilter>());
+            return await _workstationAuditService.GetSummaryByDayAndEmployeesAsync(new DataLoadingOptions<SummaryFilter>
+            {
+                Take = count,
+                SortedColumn = nameof(SummaryByDayAndEmployee.Date),
+                SortDirection = ListSortDirection.Descending
+            });
         }
 
-        //[HttpPost]
-        //[ProducesResponseType(StatusCodes.Status200OK)]
-        //public async Task<ActionResult<IEnumerable<SummaryByDayAndEmployee>>> GetFilteredSummaryByDayAndEmployees(SummaryFilter summaryFilter)
-        //{
-        //    //return await _workstationAuditService.GetFilteredSummaryByDaysAndEmployeesAsync(summaryFilter);
-        //    var list = new List<SummaryByDayAndEmployee>();
-        //    return list;
-        //}
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<SummaryByDayAndEmployee>>> GetFilteredSummaryByDayAndEmployees(SummaryFilter summaryFilter)
+        {
+            var count = await _workstationAuditService.GetSummaryByDayAndEmployeesCountAsync(new DataLoadingOptions<SummaryFilter>
+            {
+                Filter = summaryFilter
+            });
+
+            return await _workstationAuditService.GetSummaryByDayAndEmployeesAsync(new DataLoadingOptions<SummaryFilter>
+            {
+                Take = count,
+                SortedColumn = nameof(SummaryByDayAndEmployee.Date),
+                SortDirection = ListSortDirection.Descending,
+                Filter = summaryFilter
+            });
+        }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<SummaryByEmployees>>> GetSummaryByEmployee()
         {
-            //return await _workstationAuditService.GetSummaryByEmployeesAsync();
-            var list = new List<SummaryByEmployees>();
-            return list;
+            var count = await _workstationAuditService.GetSummaryByEmployeesCountAsync(new DataLoadingOptions<SummaryFilter>());
+            return await _workstationAuditService.GetSummaryByEmployeesAsync(new DataLoadingOptions<SummaryFilter>
+            {
+                Take = count,
+                SortedColumn = nameof(SummaryByEmployees.Employee),
+                SortDirection = ListSortDirection.Ascending
+            });
         }
 
-        //[HttpPost]
-        //[ProducesResponseType(StatusCodes.Status200OK)]
-        //public async Task<ActionResult<IEnumerable<SummaryByEmployees>>> GetFilteredSummaryByEmployees(SummaryFilter summaryFilter)
-        //{
-        //    //return await _workstationAuditService.GetFilteredSummaryByEmployeesAsync(summaryFilter);
-        //    var list = new List<SummaryByEmployees>();
-        //    return list;
-        //}
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<SummaryByEmployees>>> GetFilteredSummaryByEmployees(SummaryFilter summaryFilter)
+        {
+            var count = await _workstationAuditService.GetSummaryByEmployeesCountAsync(new DataLoadingOptions<SummaryFilter>
+            {
+                Filter = summaryFilter
+            });
+
+            return await _workstationAuditService.GetSummaryByEmployeesAsync(new DataLoadingOptions<SummaryFilter>
+            {
+                Take = count,
+                SortedColumn = nameof(SummaryByEmployees.Employee),
+                SortDirection = ListSortDirection.Ascending,
+                Filter = summaryFilter
+            });
+        }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<SummaryByDepartments>>> GetSummaryByDepartments()
         {
-            //return await _workstationAuditService.GetSummaryByDepartmentsAsync();
-            var list = new List<SummaryByDepartments>();
-            return list;
+            var count = await _workstationAuditService.GetSummaryByDepartmentsCountAsync(new DataLoadingOptions<SummaryFilter>());
+            return await _workstationAuditService.GetSummaryByDepartmentsAsync(new DataLoadingOptions<SummaryFilter>
+            {
+                Take = count,
+                SortedColumn = nameof(SummaryByDepartments.Company),
+                SortDirection = ListSortDirection.Ascending
+            });
         }
 
-        //[HttpPost]
-        //[ProducesResponseType(StatusCodes.Status200OK)]
-        //public async Task<ActionResult<IEnumerable<SummaryByDepartments>>> GetFilteredSummaryByDepartments(SummaryFilter summaryFilter)
-        //{
-        //    return await _workstationAuditService.GetFilteredSummaryByDepartmentsAsync(summaryFilter);
-        //}
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<SummaryByDepartments>>> GetFilteredSummaryByDepartments(SummaryFilter summaryFilter)
+        {
+            var count = await _workstationAuditService.GetSummaryByDepartmentsCountAsync(new DataLoadingOptions<SummaryFilter>
+            {
+                Filter = summaryFilter
+            });
 
+            return await _workstationAuditService.GetSummaryByDepartmentsAsync(new DataLoadingOptions<SummaryFilter>
+            {
+                Take = count,
+                SortedColumn = nameof(SummaryByDepartments.Company),
+                SortDirection = ListSortDirection.Ascending,
+                Filter = summaryFilter
+            });
+        }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<SummaryByWorkstations>>> GetSummaryByWorkstations()
         {
-            //return await _workstationAuditService.GetSummaryByWorkstationsAsync();
-            var list = new List<SummaryByWorkstations>();
-            return list;
+            var count = await _workstationAuditService.GetSummaryByWorkstationsCountAsync(new DataLoadingOptions<SummaryFilter>());
+            return await _workstationAuditService.GetSummaryByWorkstationsAsync(new DataLoadingOptions<SummaryFilter>
+            {
+                Take = count,
+                SortedColumn = nameof(SummaryByWorkstations.Workstation),
+                SortDirection = ListSortDirection.Ascending
+            });
         }
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<SummaryByWorkstations>>> GetFilteredSummaryByWorkstations(SummaryFilter summaryFilter)
         {
-            //return await _workstationAuditService.GetFilteredSummaryByWorkstationsAsync(summaryFilter);
-            var list = new List<SummaryByWorkstations>();
-            return list;
+            var count = await _workstationAuditService.GetSummaryByWorkstationsCountAsync(new DataLoadingOptions<SummaryFilter>
+            {
+                Filter = summaryFilter
+            });
+
+            return await _workstationAuditService.GetSummaryByWorkstationsAsync(new DataLoadingOptions<SummaryFilter>
+            {
+                Take = count,
+                SortedColumn = nameof(SummaryByWorkstations.Workstation),
+                SortDirection = ListSortDirection.Ascending,
+                Filter = summaryFilter
+            });
         }
     }
 }
