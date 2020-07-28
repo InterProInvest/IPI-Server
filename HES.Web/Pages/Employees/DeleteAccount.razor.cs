ce2b32305e470cc071abadf6d3bec5b3cd699ee3
CORE <namespacedevbox@gmail.com>
2020-07-28 12:20:32 +0300

    Fixed employee and employee details pages

--- HES.Web/Pages/Employees/DeleteAccount.razor.cs
+++ HES.Web/Pages/Employees/DeleteAccount.razor.cs
@@ -4,22 +4,32 @@ using HES.Core.Hubs;
 using HES.Core.Interfaces;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
 namespace HES.Web.Pages.Employees
 {
-    public partial class DeleteAccount : ComponentBase
+    public partial class DeleteAccount : ComponentBase, IDisposable
     {
         [Inject] public IEmployeeService EmployeeService { get; set; }
         [Inject] public IModalDialogService ModalDialogService { get; set; }
         [Inject] public IRemoteWorkstationConnectionsService RemoteWorkstationConnectionsService { get; set; }
         [Inject] public IToastService ToastService { get; set; }
+        [Inject] public IMemoryCache MemoryCache { get; set; }
         [Inject] public ILogger<DeleteAccount> Logger { get; set; }
         [Inject] public IHubContext<RefreshHub> HubContext { get; set; }
         [Parameter] public Account Account { get; set; }
         [Parameter] public string ConnectionId { get; set; }
+        public bool EntityBeingEdited { get; set; }
+
+        protected override void OnInitialized()
+        {
+            EntityBeingEdited = MemoryCache.TryGetValue(Account.Id, out object _);
+            if (!EntityBeingEdited)
+                MemoryCache.Set(Account.Id, Account);
+        }
 
         private async Task DeleteAccoountAsync()
         {
@@ -39,9 +49,10 @@ namespace HES.Web.Pages.Employees
             }
         }
 
-        private async Task CloseAsync()
+        public void Dispose()
         {
-            await ModalDialogService.CloseAsync();
+            if (!EntityBeingEdited)
+                MemoryCache.Remove(Account.Id);
         }
     }
 }
--- HES.Web/Pages/Employees/DeleteEmployee.razor.cs
+++ HES.Web/Pages/Employees/DeleteEmployee.razor.cs
@@ -4,24 +4,33 @@ using HES.Core.Hubs;
 using HES.Core.Interfaces;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
 namespace HES.Web.Pages.Employees
 {
-    public partial class DeleteEmployee : ComponentBase
+    public partial class DeleteEmployee : ComponentBase, IDisposable
     {
         [Inject] public IEmployeeService EmployeeService { get; set; }
         [Inject] public IModalDialogService ModalDialogService { get; set; }
         [Inject] public IToastService ToastService { get; set; }
+        [Inject] public IMemoryCache MemoryCache { get; set; }
         [Inject] public ILogger<DeleteEmployee> Logger { get; set; }
         [Inject] public IHubContext<RefreshHub> HubContext { get; set; }
         [Parameter] public Employee Employee { get; set; }
         [Parameter] public string ConnectionId { get; set; }
 
         public bool EmployeeHasVault { get; set; }
+        public bool EntityBeingEdited { get; set; }
 
+        protected override void OnInitialized()
+        {
+            EntityBeingEdited = MemoryCache.TryGetValue(Employee.Id, out object _);
+            if (!EntityBeingEdited)
+                MemoryCache.Set(Employee.Id, Employee);
+        }
 
         protected override void OnParametersSet()
         {
@@ -33,21 +42,23 @@ namespace HES.Web.Pages.Employees
             try
             {
                 await EmployeeService.DeleteEmployeeAsync(Employee.Id);
-                await HubContext.Clients.AllExcept(ConnectionId).SendAsync(RefreshPage.Employees, null);
+                await HubContext.Clients.AllExcept(ConnectionId).SendAsync(RefreshPage.Employees, Employee.Id);
                 ToastService.ShowToast("Employee removed.", ToastLevel.Success);
                 await ModalDialogService.CloseAsync();
             }
             catch (Exception ex)
             {
-                await ModalDialogService.CloseAsync();
                 Logger.LogError(ex.Message, ex);
                 ToastService.ShowToast(ex.Message, ToastLevel.Error);
+                await ModalDialogService.CancelAsync();
             }
         }
 
-        public async Task CancelAsync()
+        public void Dispose()
         {
-            await ModalDialogService.CloseAsync();
+            if (!EntityBeingEdited)
+                MemoryCache.Remove(Employee.Id);
         }
+
     }
 }
