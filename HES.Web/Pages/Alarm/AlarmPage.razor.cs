147df5c8b80f5ef1253a30f511f87b4ab5e45f9a
Sitler <vitaliy.martinyk@gmail.com>
2020-08-18 17:58:20 +0300

    Added enable/disable alarm modals

---
+++ HES.Web/Pages/Alarm/AlarmPage.razor.cs
@@ -0,0 +1,58 @@
+﻿using HES.Core.Entities;
+using HES.Core.Interfaces;
+using HES.Core.Models.Web.AppSettings;
+using HES.Core.Models.Web.Workstations;
+using HES.Core.Services;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace HES.Web.Pages.Alarm
+{
+    public partial class AlarmPage : ComponentBase
+    {
+        [Inject] public IMainTableService<Workstation, WorkstationFilter> MainTableService { get; set; }
+        [Inject] public IWorkstationService WorkstationService { get; set; }
+        [Inject] public IModalDialogService ModalDialogService { get; set; }
+        [Inject] public IAppSettingsService AppSettingsService { get; set; }
+
+
+        public AlarmState AlarmState { get; set; }
+        public int OnlineWorkstations { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetAlarmState();
+            OnlineWorkstations = RemoteWorkstationConnectionsService.WorkstationsOnlineCount();
+            await MainTableService.InitializeAsync(WorkstationService.GetWorkstationsAsync, WorkstationService.GetWorkstationsCountAsync, StateHasChanged, nameof(Workstation.IsOnline));
+        }
+
+        private async Task GetAlarmState()
+        {
+            AlarmState = await AppSettingsService.GetAlarmStateAsync();
+        }
+
+        private async Task AlarmEnableAsync()
+        {
+            RenderFragment body = (builder) =>
+            {
+                builder.OpenComponent(0, typeof(EnableAlarm));
+                builder.AddAttribute(1, nameof(EnableAlarm.CallBack), EventCallback.Factory.Create(this, GetAlarmState));
+                builder.CloseComponent();
+            };
+
+            await MainTableService.ShowModalAsync("Enable alarm", body);
+        }
+
+        private async Task DisableAlarmAsync()
+        {
+            RenderFragment body = (builder) =>
+            {
+                builder.OpenComponent(0, typeof(DisableAlarm));
+                builder.AddAttribute(1, nameof(DisableAlarm.CallBack), EventCallback.Factory.Create(this, GetAlarmState));
+                builder.CloseComponent();
+            };
+
+            await MainTableService.ShowModalAsync("Disable alarm", body);
+        }
+    }
+}
---
+++ HES.Web/Pages/Alarm/EnableAlarm.razor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using HES.Core.Enums;
+using HES.Core.Entities;
+using HES.Core.Interfaces;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace HES.Web.Pages.Alarm
+{
+    public partial class EnableAlarm : ComponentBase
+    {
+        [Inject] public IToastService ToastService { get; set; }
+        [Inject] public ILogger<EnableAlarm> Logger { get; set; }
+        [Inject] public IModalDialogService ModalDialogService { get; set; }
+        [Inject] public UserManager<ApplicationUser> UserManager { get; set; }
+        [Inject] public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+        [Inject] public IRemoteWorkstationConnectionsService RemoteWorkstationConnections { get; set; }
+
+        [Parameter] public EventCallback CallBack { get; set; }
+
+        private async Task EnableAlarmAsync()
+        {
+            var state = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            var applicationUser = await UserManager.GetUserAsync(state.User);
+
+            try
+            {
+                await RemoteWorkstationConnections.LockAllWorkstations(applicationUser);
+                ToastService.ShowToast("All workstations are successfully locked", ToastLevel.Success);
+                
+                await CallBack.InvokeAsync(this);
+                await ModalDialogService.CloseAsync();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message, ex);
+                ToastService.ShowToast(ex.Message, ToastLevel.Error);
+                await ModalDialogService.CancelAsync();
+            }
+        }
+    }
+}
