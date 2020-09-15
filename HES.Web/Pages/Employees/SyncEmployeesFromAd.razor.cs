8718492d77dda07f92b685e67ec4f2d218e68689
CORE <namespacedevbox@gmail.com>
2020-09-15 12:09:48 +0300

    Added sync employee component

---
+++ HES.Web/Pages/Employees/SyncEmployeesFromAd.razor.cs
@@ -0,0 +1,37 @@
+﻿using HES.Core.Enums;
+using HES.Core.Hubs;
+using HES.Core.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace HES.Web.Pages.Employees
+{
+    public partial class SyncEmployeesFromAd : ComponentBase
+    {
+        [Inject] public ILogger<SyncEmployeesFromAd> Logger { get; set; }
+        [Inject] public IModalDialogService ModalDialogService { get; set; }
+        [Inject] public IToastService ToastService { get; set; }
+        [Inject] public IHubContext<RefreshHub> HubContext { get; set; }
+        [Parameter] public string ConnectionId { get; set; }
+
+        private async Task SyncAsync()
+        {
+            try
+            {
+                await Task.Delay(2222);
+                // TODO run sync method
+                await HubContext.Clients.AllExcept(ConnectionId).SendAsync(RefreshPage.Employees);
+                await ModalDialogService.CloseAsync();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+                ToastService.ShowToast(ex.Message, ToastLevel.Error);
+                await ModalDialogService.CancelAsync();
+            }
+        }
+    }
+}
